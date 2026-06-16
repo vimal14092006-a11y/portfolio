162f69b109359237d033bb75a0dda26be984e2fb
vimal14092006-a11y <vimal14092006@gmail.com>
2026-06-16 22:20:26 +0530

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -2,4 +2,5 @@ const toggleBtn = document.getElementById("theme-toggle");
 
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+    console.log(document.body.className);
 });
